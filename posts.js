58a4cf697b281acdbb291ff704d617f7fd22b493
Nick K <nick.kimolas@gmail.com>
2025-08-16 16:54:37 -0400

    Blog posting functionality

---
+++ posts.js
@@ -0,0 +1,16 @@
+// To add a new post, copy the format below and add it to the top of the list.
+// The date is optional but recommended.
+const posts = [
+    {
+        title: "Welcome to my Blog",
+        date: "2025-08-16",
+        content: `This is my first post. I'll be using this space to document my journey as I study mathematics and physics.
+        
+        I'll share my progress, insights, and any interesting problems I encounter.`
+    },
+    // {
+    //     title: "Another Post",
+    //     date: "2025-08-17",
+    //     content: "This is another post."
+    // }
+];
