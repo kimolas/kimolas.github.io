f1584fb8fdaf64d745958eac415fb01698702ca1
Nick K <nicolas.kim@icloud.com>
2025-08-16 17:54:30 -0400

    Delete 3 post(s)

--- posts.js
+++ posts.js
@@ -1,26 +1,5 @@
 // To add a new post, copy the format below and add it to the top of the list.
 // The date is optional but recommended.
 const posts = [
-    {
-        title: "Testing again",
-        date: "2025-08-16",
-        content: `with cache updating thing`
-    },
-    {
-        title: "Testing",
-        date: "2025-08-16",
-        content: `testing posting functionality`
-    },
-    {
-        title: "Welcome to my Blog",
-        date: "2025-08-16",
-        content: `This is my first post. I'll be using this space to document my journey as I study mathematics and physics.
-        
-        I'll share my progress, insights, and any interesting problems I encounter.`
-    },
-    // {
-    //     title: "Another Post",
-    //     date: "2025-08-17",
-    //     content: "This is another post."
-    // }
+
 ];
