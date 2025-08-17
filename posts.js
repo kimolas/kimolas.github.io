219d23296f2970a9e945d78ea8cd6eec10293730
Nick K <nicolas.kim@icloud.com>
2025-08-16 21:11:52 -0400

    Edit blog post: QM (Hall)

--- posts.js
+++ posts.js
@@ -2,18 +2,10 @@
 // The date is optional but recommended.
 const posts = [
     {
-        title: "test1323t",
-        date: "2025-08-17",
-        content: `3qr3f3`
-    },
-    {
-        title: "test1",
-        date: "2025-08-17",
-        content: `tfewafewaf`
-    },
-    {
         title: "QM (Hall)",
         date: "2025-08-16",
-        content: `Read through section 7.1 of Hall's "QM for Mathematicians." I now understand better the relationship between the spectrum $\\sigma(A)$ and the eigenvalues (if any) of a bounded operator $A$.`
+        content: `Read through section 7.1 of Hall's "QM for Mathematicians." I now understand better the relationship between the spectrum $\\sigma(A)$ and the eigenvalues (if any) of a bounded operator $A$.
+
+testestes`
     }
 ];
