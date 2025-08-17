e18a19fe1ea8383dfc2469f738d2d61c14f85575
Nick K <nicolas.kim@icloud.com>
2025-08-16 23:39:52 -0400

    Edit blog post: QM (Hall)

--- posts.js
+++ posts.js
@@ -15,6 +15,6 @@ Proof: By first iso theorem, WTS $R/\\Ker{\\varphi}$ is local. Know $1_R \\notin
     {
         title: "QM (Hall)",
         date: "2025-08-16",
-        content: `Read through section 7.1 of Hall's "QM for Mathematicians." I now understand better the relationship between the spectrum $\\sigma(A)$ and the eigenvalues (if any) of a bounded operator $A$.`
+        content: `I read through section 7.1 of Hall's "QM for Mathematicians." I now understand better the relationship between the spectrum $\\sigma(A)$ and the eigenvalues (if any) of a bounded operator $A$.`
     }
 ];
