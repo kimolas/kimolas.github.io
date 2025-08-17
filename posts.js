d664027fedec68f6c628ac02730c8ae5cf3928f9
Nick K <nicolas.kim@icloud.com>
2025-08-17 17:49:42 -0400

    Update blog posts

--- posts.js
+++ posts.js
@@ -2,6 +2,12 @@
 // The date is optional but recommended.
 const defaultPosts = [
     {
+        "title": "Testing new post",
+        "content": "Test",
+        "createdDate": "2025-08-17",
+        "lastEdited": null
+    },
+    {
         "title": "Local rings",
         "content": "I read through Hungerford's chapter on rings. After the exercises, I have some handle on local rings, but I don't yet understand the connection to geometry---and a ring having a unique maximal ideal still feels like an arbitrary distinction. \n\nExercise 3.4.16 was entertaining. I enjoy finding ways to apply the first isomorphism theorem (that is, if $\\varphi: R \\to \\Ima {\\varphi}$ is a ring homomorphism, then $R/\\Ker{\\varphi} \\cong \\Ima{\\varphi}$).\n\nThe exercise was: prove every nonzero homomorphic image of a local ring is local. \n\nProof: By first iso theorem, WTS $R/\\Ker{\\varphi}$ is local. Know $1_R \\notin \\Ker{\\varphi}$ as that would imply $\\Ker{\\varphi} = R$, hence $\\Ima{\\varphi} = 0$, a contradiction. Now, since $\\Ima{\\varphi} \\neq 0$, we clearly have $R \\neq 0$; therefore every ideal of $R$, such as $\\Ker{\\varphi}$, is contained in a maximal ideal of $R$. But $R$ being local has a unique maximal ideal, call it $M$, whence $\\Ker{\\varphi} \\subseteq M$. Finally, we apply the correspondence theorem (AKA the fourth isomorphism theorem), which says that the ideals that contain $\\Ker{\\varphi}$ are in bijection with the ideals of $R/\\Ker{\\varphi}$, hence every ideal of $R/\\Ker{\\varphi} \\cong \\Ima{\\varphi}$ is of the form $J/\\Ker{\\varphi}$. Any such ideal must be contained in $M/\\Ker{\\varphi}$ or be $\\Ima{\\varphi}$ itself, hence $M/\\Ker{\\varphi}$ is the unique maximal ideal, and so $\\Ima{\\varphi}$ is local. $\\blacksquare$",
         "createdDate": "2025-08-16",
